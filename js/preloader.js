ece00c6af637e1922b755ccfadaa0e7492ce39be
negativepriority <negativepriority@users.noreply.github.com>
2024-01-04 16:00:45 +0100

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -3,6 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
   const preloaderImage = preloader.querySelector('img');
   const body = document.body;
 
+  // Funzione per salvare lo stato della GIF nel localStorage
+  function saveGifState() {
+      localStorage.setItem('preloaderGifState', preloaderImage.src);
+  }
+
+  // Funzione per ripristinare lo stato della GIF dal localStorage
+  function restoreGifState() {
+      const savedState = localStorage.getItem('preloaderGifState');
+      if (savedState) {
+          preloaderImage.src = savedState;
+      }
+  }
+
   // Funzione per reimpostare la GIF al primo frame
   function resetGif() {
       preloaderImage.src = preloaderImage.src;
@@ -17,19 +30,22 @@ document.addEventListener('DOMContentLoaded', function () {
               preloader.style.display = 'none'; // Nascondi il preloader dopo l'animazione
           }, 500); // 500 millisecondi = 0.5 secondi
       }, 2050); // 2100 millisecondi = 2.3 secondi
+
+      // Salva lo stato della GIF quando la pagina sta per essere scaricata
+      saveGifState();
   }
 
   // Attendi il caricamento completo della pagina prima di rimuovere la classe 'loading'
-  window.addEventListener('load', handlePageLoad);
+  window.addEventListener('load', function () {
+      // Ripristina lo stato della GIF dal localStorage
+      restoreGifState();
 
-  // Gestisci l'evento popstate quando si torna indietro nella cronologia del browser
-  window.addEventListener('popstate', function (event) {
-      // Verifica se l'evento popstate è dovuto al back/forward del browser
-      if (event.state && event.state.pageReloaded) {
-          // Resetta la GIF al primo frame quando la pagina viene ripresa dalla cache
-          resetGif();
-      }
+      // Gestisci il caricamento della pagina e la rimozione del preloader
+      handlePageLoad();
   });
+
+  // Gestisci l'evento unload per salvare lo stato della GIF prima che la pagina venga scaricata
+  window.addEventListener('unload', saveGifState);
 
   // Resetta la GIF al primo frame quando il preloader viene nascosto
   preloader.addEventListener('transitionend', resetGif);
@@ -41,8 +57,8 @@ document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('pageshow', function (event) {
       // Verifica se l'evento pageshow è dovuto al back/forward del browser
       if (event.persisted) {
-          // Resetta la GIF al primo frame quando la pagina viene ripresa dalla cache
-          resetGif();
+          // Ripristina lo stato della GIF dal localStorage
+          restoreGifState();
       }
   });
 });
