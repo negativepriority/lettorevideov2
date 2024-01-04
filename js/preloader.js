14ada1dd5d605b2f7cef14d0fdfe146b67eda065
negativepriority <negativepriority@users.noreply.github.com>
2024-01-04 14:53:27 +0100

    preloader fix

--- js/preloader.js
+++ js/preloader.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const preloader = document.getElementById('preloader');
-    const body = document.body;
+  const preloader = document.getElementById('preloader');
+  const preloaderImage = preloader.querySelector('img');
+  const body = document.body;
 
-    // Attendi il caricamento completo del sito prima di rimuovere la classe 'loading'
-    window.addEventListener('load', function () {
+  // Funzione per reimpostare la GIF al primo frame
+  function resetGif() {
+      preloaderImage.src = preloaderImage.src;
+  }
+
+  // Attendi il caricamento completo del sito prima di rimuovere la classe 'loading'
+  window.addEventListener('load', function () {
       setTimeout(function () {
-        body.classList.remove('loading');
-        preloader.style.opacity = '0'; // Riduci l'opacità del preloader
-        setTimeout(function () {
-          preloader.style.display = 'none'; // Nascondi il preloader dopo l'animazione
-        }, 500); // 500 millisecondi = 0.5 secondi
+          body.classList.remove('loading');
+          preloader.style.opacity = '0'; // Riduci l'opacità del preloader
+          setTimeout(function () {
+              preloader.style.display = 'none'; // Nascondi il preloader dopo l'animazione
+          }, 500); // 500 millisecondi = 0.5 secondi
       }, 2050); // 2100 millisecondi = 2.3 secondi
-    });
+
+      // Resetta la GIF al primo frame ogni volta che il preloader viene visualizzato
+      preloader.addEventListener('transitionend', resetGif);
   });
+});
