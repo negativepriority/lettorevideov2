84ee278575988960da6bd9b2e75359057b4d3011
negativepriority <negativepriority@users.noreply.github.com>
2024-08-08 16:30:43 +0200

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -3,10 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const preloaderImage = document.getElementById('preloader-img');
     const body = document.body;
 
+    // Funzione per reimpostare la GIF al primo frame
+    function resetGif() {
+        // Solo se la GIF è visibile
+        if (preloader.style.display === 'flex') {
+            const gifSrc = preloaderImage.src;
+            preloaderImage.src = '';  // Rimuove temporaneamente la src
+            preloaderImage.src = gifSrc + '?' + new Date().getTime(); // Riaggiunge la src con un timestamp per evitare la cache
+        }
+    }
+
     // Funzione per mostrare il preloader
     function showPreloader() {
         preloader.style.display = 'flex';
         preloader.style.opacity = '1';
+        resetGif(); // Resetta la GIF solo quando il preloader viene mostrato
         body.classList.add('loading');
     }
 
@@ -14,11 +25,11 @@ document.addEventListener('DOMContentLoaded', function () {
     function hidePreloader() {
         setTimeout(function () {
             body.classList.remove('loading');
-            preloader.style.opacity = '0';
+            preloader.style.opacity = '0'; // Riduci l'opacità del preloader
             setTimeout(function () {
-                preloader.style.display = 'none';
-            }, 500);
-        }, 2050);
+                preloader.style.display = 'none'; // Nascondi il preloader dopo l'animazione
+            }, 500); // 500 millisecondi = 0.5 secondi
+        }, 2050); // 2050 millisecondi = 2.05 secondi
     }
 
     // Attendi il caricamento completo della pagina prima di rimuovere la classe 'loading'
@@ -27,8 +38,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Gestisci l'evento popstate quando si torna indietro nella cronologia del browser
     window.addEventListener('popstate', function () {
         showPreloader();
-        hidePreloader();
+        // Non nascondere il preloader qui, si nasconderà nel pageshow
     });
+
+    // Aggiungi un nuovo elemento di stato per indicare il ricaricamento della pagina
+    history.replaceState({ pageReloaded: true }, '');
 
     // Ascolta l'evento pageshow per gestire il ricaricamento della pagina
     window.addEventListener('pageshow', function (event) {
@@ -37,4 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
             hidePreloader();
         }
     });
+
+    // Mostra il preloader al caricamento iniziale
+    showPreloader();
 });
