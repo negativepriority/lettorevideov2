e129be3071b0f44c06f4041f65030a228559c55f
negativepriority <negativepriority@users.noreply.github.com>
2024-08-08 16:08:30 +0200

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -3,17 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const preloaderImage = document.getElementById('preloader-img');
     const body = document.body;
 
-    // Funzione per reimpostare la GIF al primo frame senza rimuovere temporaneamente la src
-    function resetGif() {
-        const gifSrc = preloaderImage.src;
-        preloaderImage.src = gifSrc + '?' + new Date().getTime(); // Aggiorna la src con un timestamp per evitare la cache
-    }
-
     // Funzione per mostrare il preloader
     function showPreloader() {
         preloader.style.display = 'flex';
         preloader.style.opacity = '1';
-        resetGif(); // Resetta la GIF solo quando il preloader viene mostrato
         body.classList.add('loading');
     }
 
@@ -25,26 +18,28 @@ document.addEventListener('DOMContentLoaded', function () {
             setTimeout(function () {
                 preloader.style.display = 'none'; // Nascondi il preloader dopo l'animazione
             }, 500); // 500 millisecondi = 0.5 secondi
-        }, 2050); // 2050 millisecondi = 2.05 secondi
+        }, 2000); // Ridotto a 2 secondi per evitare ritardi nel nascondere il preloader
     }
 
     // Attendi il caricamento completo della pagina prima di rimuovere la classe 'loading'
     window.addEventListener('load', hidePreloader);
 
     // Gestisci l'evento popstate quando si torna indietro nella cronologia del browser
-    window.addEventListener('popstate', function (event) {
+    window.addEventListener('popstate', function () {
+        preloaderImage.src = preloaderImage.src.split("?")[0] + '?' + new Date().getTime();
         showPreloader();
+        hidePreloader();
     });
 
     // Ascolta l'evento pageshow per gestire il ricaricamento della pagina
     window.addEventListener('pageshow', function (event) {
-        // Verifica se l'evento pageshow è dovuto al back/forward del browser o da cache
         if (event.persisted || performance.getEntriesByType('navigation')[0].type === 'back_forward') {
+            preloaderImage.src = preloaderImage.src.split("?")[0] + '?' + new Date().getTime();
             showPreloader();
             hidePreloader();
         }
     });
 
-    // Resetta la GIF al primo frame quando il DOM è pronto
-    resetGif();
+    // Mostra il preloader all'avvio
+    showPreloader();
 });
