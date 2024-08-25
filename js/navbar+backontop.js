daef776d58fadaede3bc5a63fb068951a150320f
negativepriority <negativepriority@users.noreply.github.com>
2024-08-25 15:25:52 +0200

    navbar final fix

--- js/navbar+backontop.js
+++ js/navbar+backontop.js
@@ -1,42 +1,61 @@
-//No scroll da telefono quando l'utente clicca il menù hamburger della nav
-document.addEventListener('DOMContentLoaded', function () {
-  const checkBtn = document.getElementById('check');
-  const body = document.body;
-  const html = document.documentElement;
+/*
+ * Original Navbar JavaScript Author : Oğuzhan Avcı
+ * https://github.com/oziavci
+ * Andrea Capizzi's custom version.
+*/
 
-  checkBtn.addEventListener('change', function () {
-    if (checkBtn.checked) {
-      body.classList.add('no-scroll');
-      // blocco per Firefox
-      body.style.overflow = 'hidden';
-      html.style.overflow = 'hidden';
-      body.style.height = '100%';
-    } else {
-      body.classList.remove('no-scroll');
-      // Ablocco per Firefox
-      body.style.overflow = '';
-      html.style.overflow = '';
-      body.style.height = '';
-    }
-  });
+const openMenu = document.querySelector(".menu-open");
+const closeMenu = document.querySelector(".menu-close");
+const menuDiv = document.querySelector(".ozmenu");
+const menu = document.querySelector(".ozmenu-nav");
+const dropDowns = menu.getElementsByClassName("nav-dropdown");
+const dropDownsChild = menu.querySelectorAll('.dropdown .nav-dropdown');
+
+// Aggiungi la classe per identificare l'interazione del menu
+openMenu.addEventListener("click", function() {
+    menuToggle();
+    document.body.classList.add('menu-interaction'); // Aggiungi la classe per segnalare l'interazione con il menu
 });
 
-//Nella nav il menù hamburger diventa una X e viceversa
-document.addEventListener('DOMContentLoaded', function () {
-          const checkBtn = document.getElementById('check');
-          const iconToggle = document.getElementById('icon-toggle');
+closeMenu.addEventListener("click", function() {
+    menuToggle();
+    document.body.classList.remove('menu-interaction'); // Rimuovi la classe quando il menu viene chiuso
+});
 
-          checkBtn.addEventListener('change', function () {
-              if (checkBtn.checked) {
-                  iconToggle.classList.replace('fa-bars', 'fa-times');
-              } else {
-                  iconToggle.classList.replace('fa-times', 'fa-bars');
-              }
-          });
-      });
+document.body.insertAdjacentHTML("beforeend", "<div id='menu-overlay'></div>");
+document.querySelector("#menu-overlay").addEventListener("click", function() {
+    menuToggle();
+    document.body.classList.remove('menu-interaction'); // Rimuovi la classe quando si clicca fuori dal menu
+});
 
+function menuToggle() {
+    menuDiv.classList.toggle("active");
+    document.body.classList.toggle("hide-scrolling");
+    document.body.classList.toggle("mobile-menu-active");
+    document.getElementById("menu-overlay").classList.toggle("show");
+}
 
-      // Get the button:
+for (var i = 0; i < dropDownsChild.length; i++) {
+    dropDownsChild[i].classList.add('child');
+    dropDownsChild[i].addEventListener("click", function() {
+        this.classList.toggle('opened');
+    });
+}
+
+for (var i = 0; i < dropDowns.length; i++) {
+    if(!dropDowns[i].classList.contains("child")){
+        dropDowns[i].classList.add('parent');
+        dropDowns[i].addEventListener("click", function() {
+            this.classList.toggle('opened');
+        });
+    }
+}
+
+//----------------------------------------------------------------------------
+
+// Back on top script:
+
+// Get the button:
 let mybutton = document.getElementById("mybackontopBtn");
 
 // When the user scrolls down 150px from the top of the document, show the button
--- js/preloader.js
+++ js/preloader.js
@@ -3,52 +3,47 @@ document.addEventListener('DOMContentLoaded', function () {
     const preloaderImage = document.getElementById('preloader-img');
     const body = document.body;
 
-    // Funzione per reimpostare la GIF al primo frame
     function resetGif() {
         const gifSrc = preloaderImage.src;
-        preloaderImage.src = '';  // Rimuove temporaneamente la src
-        preloaderImage.src = gifSrc + '?' + new Date().getTime(); // Riaggiunge la src con un timestamp per evitare la cache
+        preloaderImage.src = '';
+        preloaderImage.src = gifSrc + '?' + new Date().getTime();
     }
 
-    // Funzione per mostrare il preloader
     function showPreloader() {
-        preloader.style.display = 'flex';
-        preloader.style.opacity = '1';
-        resetGif(); // Resetta la GIF solo quando il preloader viene mostrato
-        body.classList.add('loading');
+        if (!body.classList.contains('menu-interaction')) { // Mostra il preloader solo se non è in corso un'interazione con il menu
+            preloader.style.display = 'flex';
+            preloader.style.opacity = '1';
+            resetGif();
+            body.classList.add('loading');
+        }
     }
 
-    // Funzione per nascondere il preloader dopo il caricamento della pagina
     function hidePreloader() {
         setTimeout(function () {
             body.classList.remove('loading');
-            preloader.style.opacity = '0'; // Riduci l'opacità del preloader
+            preloader.style.opacity = '0';
             setTimeout(function () {
-                preloader.style.display = 'none'; // Nascondi il preloader dopo l'animazione
-            }, 500); // 500 millisecondi = 0.5 secondi
-        }, 2050); // 2050 millisecondi = 2.05 secondi
+                preloader.style.display = 'none';
+            }, 500);
+        }, 2050);
     }
 
-    // Attendi il caricamento completo della pagina prima di rimuovere la classe 'loading'
     window.addEventListener('load', hidePreloader);
 
-    // Gestisci l'evento popstate quando si torna indietro nella cronologia del browser
     window.addEventListener('popstate', function (event) {
-        showPreloader();
+        if (!event.state || !event.state.menuInteraction) {
+            showPreloader();
+        }
     });
 
-    // Aggiungi un nuovo elemento di stato per indicare il ricaricamento della pagina
-    history.replaceState({ pageReloaded: true }, '');
-
-    // Ascolta l'evento pageshow per gestire il ricaricamento della pagina
     window.addEventListener('pageshow', function (event) {
-        // Verifica se l'evento pageshow è dovuto al back/forward del browser o da cache
         if (event.persisted || performance.getEntriesByType('navigation')[0].type === 'back_forward') {
             showPreloader();
             hidePreloader();
         }
     });
 
-    // Resetta la GIF al primo frame quando il DOM è pronto
+    history.replaceState({ pageReloaded: true }, '');
+
     resetGif();
 });
